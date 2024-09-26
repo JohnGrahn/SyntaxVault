3ce9a755244613777111dd85fafa355d82e03725
John Grahn <johngrahn@protonmail.com>
2024-09-25 21:37:11 -0400

    Initial commit

---
+++ syntaxvault-backend/src/main/java/com/syntaxvault/SyntaxvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.syntaxvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntaxvaultBackendApplication.class, args);
+	}
+
+}
---
+++ syntaxvault-backend/src/test/java/com/syntaxvault/SyntaxvaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntaxvaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
